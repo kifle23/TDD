60eb3112db74288131bd96ed7510e0c0611cba3e
kifle23 <kiflekifle23@gmail.com>
2023-08-03 13:57:30 +0300

    Add method factorial for Solver class

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,3 +1,7 @@
 class Solver
-   
+    def factorial(n)
+        raise ArgumentError, "Argument must be a non-negative integer" if n < 0
+        return 1 if n == 0
+        (1..n).reduce(:*)
+    end
 end

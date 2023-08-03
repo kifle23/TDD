7ee9ac2d30d63b4d730c70ffbf2170cc35f118f6
kifle23 <kiflekifle23@gmail.com>
2023-08-03 13:54:21 +0300

    Add test case for Solver#factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/solver'
 
 describe Solver do
-    
+    describe '#factorial' do
+        it 'returns 1 for 0' do
+            expect(subject.factorial(0)).to eq(1)
+        end
+      
+        it 'returns the factorial for a positive integer' do
+            expect(subject.factorial(5)).to eq(120)
+         end
+      
+        it 'raises an exception for a negative integer' do
+            expect { subject.factorial(-1) }.to raise_error(ArgumentError)
+        end 
+    end
 end
